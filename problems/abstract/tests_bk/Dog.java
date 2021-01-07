0b7a329825035dc73577ea99d6458b66f5d4f5c3
SurgeArrester <cameron.h@rgreaves.me.uk>
2021-01-07 13:05:15 +0000

    Got interface test working

--- problems/abstract/tests_bk/Dog.java
+++
@@ -1,9 +0,0 @@
-public class Dog implements Degreeable {
-    boolean goodGirl = true;
-
-    public void awardDegree() {
-        if (goodGirl) {
-            System.out.println("Arruff woof woof. WOOF!");
-        }
-    }
-}
